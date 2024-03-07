7d96fa46867e6f511cd9847bf90af7374ba74d71
Robert Medina <robertsmedina11@gmail.com>
2024-03-06 20:15:34 -0500

    challenge 7

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Home from './Home'
 import Form from './Form'
-import { BrowserRouter as Router,  NavLink, Route,Routes } from 'react-router-dom'
+import { BrowserRouter as   NavLink, Route,Routes } from 'react-router-dom'
 
 function App() {
   
